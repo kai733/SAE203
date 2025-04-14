2ce03d600c1227811c867c992f1aa26ef352b699
OraSaNe <ORASANEDEL@gmail.com>
2025-04-14 09:37:15 +0200

    base site

---
+++ Site/index_user.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <p>Bibliothèque............................</p>
+        <p>Accueil /</p>
+        <hr>
+            <li>
+                <a href="index.html">Accueil</a>
+            </li>
+            <li>
+                <a href="categories.html">Catégories</a>
+            </li>
+            <li>
+                <a href="nouveautés.html">Nouveautés</a>
+            </li>
+            <li>
+                <a href="cdc.html">Coup de Coeur</a>
+            </li>
+    </header>
+    <div>
+        <p>Trier</p>
+    </div>
+    <div class="container">
+            <h1>Titre</h1>
+         <h3>Catégorie</h3>
+         <h5>Non disponible</h5>
+        <div class="separateur"> </div>   
+        <h3>Description</h3>
+        <p>L'Odyssée d'Homère narre le périple épique d'Ulysse après la guerre de Troie. Son voyage de retour à Ithaque est entravé par des épreuves surnaturelles, notamment chez les Cyclopes, les Sirènes et Circé. Pendant son absence, sa femme Pénélope résiste aux prétendants.
+         </p>
+    </div>
+    <div class="espace"></div>
+    
+</body>
+</html>

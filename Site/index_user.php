42d12b2c05ba081c8a56891d97ac974fd44394b9
OraSaNe <ORASANEDEL@gmail.com>
2025-04-14 10:24:09 +0200

    Update index_user.php

--- Site/index_user.php
+++ Site/index_user.php
@@ -27,6 +27,7 @@
         <p>Trier</p>
     </div>
     <div class="container">
+        <img src="images/livre_disponible.png" alt="Livre disponible" class="image_livre">
             <h1>Titre</h1>
          <h3>Catégorie</h3>
          <h5>Non disponible</h5>
@@ -35,7 +36,56 @@
         <p>L'Odyssée d'Homère narre le périple épique d'Ulysse après la guerre de Troie. Son voyage de retour à Ithaque est entravé par des épreuves surnaturelles, notamment chez les Cyclopes, les Sirènes et Circé. Pendant son absence, sa femme Pénélope résiste aux prétendants.
          </p>
     </div>
+
     <div class="espace"></div>
-    
+
+    <div class="non_disponible">
+        <div>
+            <img src="images/livre_non_disponible.png" alt="Livre non disponible" class="image_livre">
+        </div>
+        <h3>Titre</h3>
+        <p>Catégorie</p>
+        <p>Non disponible</p>
+    </div>
+
+    <div class="disponible">
+        <div>
+            <img src="images/livre_disponible.png" alt="Livre non disponible" class="image_livre">
+        </div>
+        <h3>Titre</h3>
+        <p>Catégorie</p>
+        <p>Disponible</p>
+    </div>
+
+    <div class="espace"></div>
+
+    <div class="container">
+        <img src="images/livre_non_disponible.png" alt="Livre non disponible" class="image_livre">
+            <h1>Titre</h1>
+         <h3>Catégorie</h3>
+         <h5>Non disponible</h5>
+        <div class="separateur"> </div>   
+        <h3>Description</h3>
+        <p>L'Odyssée d'Homère narre le périple épique d'Ulysse après la guerre de Troie. Son voyage de retour à Ithaque est entravé par des épreuves surnaturelles, notamment chez les Cyclopes, les Sirènes et Circé. Pendant son absence, sa femme Pénélope résiste aux prétendants.
+         </p>
+    </div>
+    <div class="non_disponible">
+        <div>
+            <img src="images/livre_non_disponible.png" alt="Livre non disponible" class="image_livre">
+        </div>
+        <h3>Titre</h3>
+        <p>Catégorie</p>
+        <p>Non disponible</p>
+    </div>
+
+    <div class="disponible">
+        <div>
+            <img src="images/livre_disponible.png" alt="Livre non disponible" class="image_livre">
+        </div>
+        <h3>Titre</h3>
+        <p>Catégorie</p>
+        <p>Disponible</p>
+    </div>
+
 </body>
 </html>
